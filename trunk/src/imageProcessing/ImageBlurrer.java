d33061b6fc391b415890a8c6abfb4b531e47cdc6
cncplyr <cncplyr@5fb41f04-2cac-e3f9-1cad-a539a558f6d6>
2010-12-30 18:29:44 +0000

    Refactored blur functions out.

---
+++ trunk/src/imageProcessing/ImageBlurrer.java
@@ -0,0 +1,47 @@
+package imageProcessing;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
+
+public class ImageBlurrer {
+
+	public ImageBlurrer() {
+
+	}
+
+	/**
+	 * Simple average blur.
+	 * 
+	 * @param img
+	 *            The image to blur.
+	 * @param radius
+	 *            The radius of the blur to apply.
+	 * @return The Blurred Image.
+	 */
+	public BufferedImage averageBlur(BufferedImage img, int radius) {
+		float[] matrix = createAverageMatrix(radius);
+
+		BufferedImageOp averageBlurOp = new ConvolveOp(new Kernel(radius, radius, matrix));
+		return averageBlurOp.filter(img, null);
+	}
+
+	/**
+	 * Makes a square matrix, where the sum of the contents adds up to 1.
+	 * 
+	 * @param size
+	 *            The size of one side of the matrix.
+	 * @return A matrix.
+	 */
+	public float[] createAverageMatrix(int size) {
+		int cells = size * size;
+		float[] matrix = new float[cells];
+
+		for (int i = 0; i < cells; i++) {
+			matrix[i] = 1.0f / (float) cells;
+		}
+
+		return matrix;
+	}
+}
