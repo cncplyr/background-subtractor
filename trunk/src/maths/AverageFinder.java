86504ed923d55830aed9f804749ad34e04d154ef
cncplyr <cncplyr@5fb41f04-2cac-e3f9-1cad-a539a558f6d6>
2010-12-31 15:51:56 +0000

    Method for calculating mode average, and argument checks for all average types.

--- trunk/src/maths/AverageFinder.java
+++ trunk/src/maths/AverageFinder.java
@@ -3,9 +3,18 @@ package maths;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 
+ * @author cncplyr
+ * @version 0.2
+ */
 public class AverageFinder {
 
 	public int findMedian(List<Integer> numbers) {
+		if (numbers.size() < 1) {
+			throw new IllegalArgumentException(
+					"Cannot find average(median) of an empty list of numbers!");
+		}
 		int size = numbers.size();
 		Collections.sort(numbers);
 		if (size % 2 == 1) {
@@ -18,16 +27,38 @@ public class AverageFinder {
 	}
 
 	public int findMean(List<Integer> numbers) {
+		if (numbers.size() < 1) {
+			throw new IllegalArgumentException(
+					"Cannot find average(mean) of an empty list of numbers!");
+		}
 		int mean = 0;
 		for (int number : numbers) {
 			mean += number;
 		}
 		return mean / numbers.size();
 	}
-	
-	public int findMode(List<Integer> numbers){
-		// TODO: Can't be bothered...
-		return 0;
+
+	public int findMode(List<Integer> numbers) {
+		if (numbers.size() < 1) {
+			throw new IllegalArgumentException(
+					"Cannot find average(mode) of an empty list of numbers!");
+		}
+		Collections.sort(numbers);
+		int currentMode = 0;
+		int currentModeCount = 0;
+		int previous = 0; // may cause a problem
+
+		for (int currentNumber : numbers) {
+			if (currentNumber != previous) {
+				int frequency = Collections.frequency(numbers, currentNumber);
+				if (frequency > currentModeCount) {
+					currentMode = currentNumber;
+					currentModeCount = frequency;
+				}
+				previous = currentNumber;
+			}
+		}
+		return currentMode;
 	}
 
 }
