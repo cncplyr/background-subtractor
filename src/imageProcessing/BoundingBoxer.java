6039ce010c38a4d2ae56d0f956036f4fcf4194aa
cncplyr <cncplyr@5fb41f04-2cac-e3f9-1cad-a539a558f6d6>
2011-01-20 23:38:17 +0000

    Framework for bounding box.

---
+++ src/imageProcessing/BoundingBoxer.java
@@ -0,0 +1,30 @@
+package imageProcessing;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * 
+ * @author Ric
+ * @version 0.1
+ * 
+ */
+public class BoundingBoxer {
+
+	public BoundingBoxer() {
+
+	}
+
+	/*
+	 * Returns the (x,y) coordinates of the top left and bottom right of the
+	 * bounding box.
+	 */
+	public int[] getBoundingBox(BufferedImage inputImage) {
+		int[] boundingBox = new int[2];
+
+		// Implement some kind of search here
+
+
+		return boundingBox;
+	}
+
+}
