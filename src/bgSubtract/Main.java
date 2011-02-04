eaef574144c3bb04d06ed49930d354db4d2eaa30
cncplyr <cncplyr@5fb41f04-2cac-e3f9-1cad-a539a558f6d6>
2011-02-04 18:14:53 +0000

    Refactored Main to work better with a gui. Started work on a gui.

--- src/bgSubtract/Main.java
+++ src/bgSubtract/Main.java
@@ -6,28 +6,42 @@ import java.awt.image.BufferedImage;
  * This is the main entry point to the program.
  * 
  * @author cncplyr
- * @version 0.2
+ * @version 0.3
  * 
  */
 public class Main {
 	private static BackgroundCreator backgroundCreator;
 	private static BackgroundSubtractor backgroundSubtractor;
-	
+
 	public static void main(String[] args) {
 		backgroundCreator = new BackgroundCreator();
 		backgroundSubtractor = new BackgroundSubtractor();
 
-		/* Create Background */
+		createBackground();
+		subtractBackground();
+
+		System.exit(0);
+	}
+
+	/**
+	 * Creates the background.
+	 */
+	public static void createBackground() {
+		BufferedImage background;
 		try {
-			BufferedImage background = backgroundCreator.createBackground();
+			background = backgroundCreator.createBackground();
 			backgroundSubtractor.setBackgroundImage(background);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		/* Subtract background from each image */
-		backgroundSubtractor.subtractAll();
-
-		System.exit(0);
 	}
+
+	/**
+	 * Subtracts background from each image.
+	 */
+	public static void subtractBackground() {
+		backgroundSubtractor.subtractAll();
+	}
+
+
 }
