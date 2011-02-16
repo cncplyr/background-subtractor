1115cfd1d485f2a766262cf16d45024a90a7e417
cncplyr <cncplyr@5fb41f04-2cac-e3f9-1cad-a539a558f6d6>
2011-02-16 11:15:46 +0000

    Added library and class for csv handling (may be needed later).

---
+++ trunk/src/fileHandling/CSVHandler.java
@@ -0,0 +1,54 @@
+package fileHandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
+/**
+ * Uses openCSV 2.2 - http://opencsv.sourceforge.net/
+ * 
+ * Currently just example code here.
+ * 
+ * @author cncplyr
+ * @version 0.1
+ * 
+ */
+public class CSVHandler {
+
+	public void read() {
+		CSVReader reader;
+		try {
+			reader = new CSVReader(new FileReader("yourfile.csv"));
+			String[] nextLine;
+			while ((nextLine = reader.readNext()) != null) {
+				// nextLine[] is an array of values from the line
+				System.out.println(nextLine[0] + nextLine[1] + "etc...");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	public void write() {
+		CSVWriter writer;
+		try {
+			writer = new CSVWriter(new FileWriter("yourfile.csv"), '\t');
+			// feed in your array (or convert your data to an array)
+			String[] entries = "first#second#third".split("#");
+			writer.writeNext(entries);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+}
