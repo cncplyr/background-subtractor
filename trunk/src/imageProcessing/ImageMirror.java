3fc24abdbe41521fbac23c7e8e34a4a645b4b685
cncplyr <cncplyr@5fb41f04-2cac-e3f9-1cad-a539a558f6d6>
2011-03-31 00:40:36 +0000

    Now outputs into desired file structure. Generates second player images (mirrored).

---
+++ trunk/src/imageProcessing/ImageMirror.java
@@ -0,0 +1,22 @@
+package imageProcessing;
+
+import java.awt.image.BufferedImage;
+
+public class ImageMirror {
+
+	public ImageMirror() {
+
+	}
+
+	public BufferedImage genFrame(BufferedImage inFrame) {
+		int width = inFrame.getWidth();
+		int height = inFrame.getHeight();
+		BufferedImage outFrame = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		
+		for (int col = 0; col < width - 1; col++) {
+			outFrame.setRGB(width - col - 1, 0, 1, height, inFrame.getRGB(col, 0, 1, height, null, 0, 1), 0, 1);
+		}
+
+		return outFrame;
+	}
+}
